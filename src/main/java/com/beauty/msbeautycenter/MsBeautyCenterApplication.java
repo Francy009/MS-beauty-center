902d8595e00f76a30934e8412b013c92bf61aa26
Anna Francesca <bagordofrancesca587@gmail.com>
2024-09-29 15:20:51 +0200

    first commit

---
+++ src/main/java/com/beauty/msbeautycenter/MsBeautyCenterApplication.java
@@ -0,0 +1,13 @@
+package com.beauty.msbeautycenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBeautyCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBeautyCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/beauty/msbeautycenter/MsBeautyCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beauty.msbeautycenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBeautyCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
