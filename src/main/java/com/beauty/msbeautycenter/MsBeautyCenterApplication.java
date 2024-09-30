113487afbdba6c08d9f2c6989f7b802039255d77
Anna Francesca <bagordofrancesca587@gmail.com>
2024-09-30 20:37:19 +0200

    aggiunta configuraizone per chiamate esterne

--- src/main/java/com/beauty/msbeautycenter/MsBeautyCenterApplication.java
+++ src/main/java/com/beauty/msbeautycenter/MsBeautyCenterApplication.java
@@ -1,7 +1,10 @@
 package com.beauty.msbeautycenter;
 
+import com.beauty.msbeautycenter.config.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MsBeautyCenterApplication {
@@ -10,4 +13,13 @@ public class MsBeautyCenterApplication {
         SpringApplication.run(MsBeautyCenterApplication.class, args);
     }
 
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+        FilterRegistrationBean<CorsFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new CorsFilter());
+        registration.addUrlPatterns("/*");
+        registration.setName("corsFilter");
+        registration.setOrder(1);
+        return registration;
+    }
 }
